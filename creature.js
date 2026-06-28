3f063ac907167ec0393a2c91182dabe6a17c460f
whenthesilly <whenthesilly@users.noreply.github.com>
2026-06-28 19:53:13 +0000

    deploy: d4614e44126f3bf9a7f1d0df1d29c5dabb1423d5

---
+++ creature.js
@@ -0,0 +1,41 @@
+// code stolen (with permission) from https://tauon.dev
+
+const KONAMI = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+    "Enter",
+  ];
+
+  let codes = [KONAMI];
+  let keys = [];
+  let codes_that_can_be_at_this_point = codes;
+  
+    
+  window.addEventListener("keydown", (event) => {
+    keys.push(event.key);
+    codes_that_can_be_at_this_point = [];
+    for (let code of codes) {
+      let is_real = true;
+      for (let key in keys) {
+        is_real = keys[key] == code[key]
+      }
+      if (is_real) codes_that_can_be_at_this_point.push(code);
+      if (is_real && code.length == keys.length) {
+        keys = [];
+        if (code == KONAMI) {
+          console.log("hiiiiii :3");
+          document.querySelector(".creature").style.display = "inline";
+       
+        }
+      }
+    }
+    if (codes_that_can_be_at_this_point.length == 0) keys = [];
+  });
